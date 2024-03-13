47c11a39f59c0b0eb719ccac3a478d829d53a7f0
manuel <manuelgonzalezcanizares@gmail.com>
2024-03-13 21:07:06 +0100

    primer commit c++

---
+++ material_claseC/Holamundo.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "iostream"
+
+using namespace std;
+
+#define PI 3.1415926536
+
+//esto es un comentario
+
+/*
+esto es un comentario mas largo.
+todo esto viene ignorado por el compilador.
+*/
+
+
+//este programa escribe Hola mundo en el output estandar, i.e. la terminal. 
+
+int main()
+{
+	printf("*************\n");
+	printf("Hola mundo!\n");//funcion C
+	printf("*************\n");
+	//cout << "Hola Mundo!!!\n";//funcion C++
+}
+  
